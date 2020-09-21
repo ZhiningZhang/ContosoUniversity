f0a7959f29652bafc55ddaf0a1de36d3dfaeadd3
Zhining Zhang <zzhang@timios.com>
2020-09-21 10:32:58 -0700

    doen step 1

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -33,7 +33,8 @@ namespace ContosoUniversity
                 try
                 {
                     var context = services.GetRequiredService<SchoolContext>();
-                    context.Database.EnsureCreated();
+                    //context.Database.EnsureCreated();
+                    DbInitializer.Initialize(context);
                 }
                 catch (Exception ex)
                 {
